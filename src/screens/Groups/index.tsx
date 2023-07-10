20a4c6a72fb93fb6b6c9c59a2d330b04ff0a44c6
devEliasJr <eliasengmecanica@gmail.com>
2023-07-10 19:55:11 -0300

    feat: Inclusao do Header HighLight padrao

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,14 @@
 import { Header } from "@components/Header";
-import { Container} from "./styles";
+import { Container } from "./styles";
+import { Highlight } from "@components/Highlight";
+import { GroupCard } from "@components/GroupCard";
 
 export function Groups() {
   return (
     <Container>
-      <Header/>
+      <Header />
+      <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
+      <GroupCard title="Galera do Ignite" />
     </Container>
   );
 }
